60ff2c226b6fc9d41f5555710f5abebd9951226a
CodyPfrunder <107026555+CodyPfrunder@users.noreply.github.com>
2022-11-28 16:26:28 -0800

    Animation, SceneBuild, GroundFix
    Progress towards project requirements

--- JimmyPants/Assets/Scripts/PlayerController.cs
+++ JimmyPants/Assets/Scripts/PlayerController.cs
@@ -3,55 +3,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody2D rb;  //The rigid body is a Unity class that is used for physics objects.
+    private Rigidbody2D player;  //The rigid body is a Unity class that is used for physics objects.
                              //We can apply forces to move a rigidbody.
     [SerializeField]
     private float force;   //Force to apply to the rigidbody
+    
+    private float xInp = 0f; //Movement left right
+
+    //Make sure player is in contact with floor to jump
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer; //specify layer "ground"
     private bool surfaced; //Player in contact with floor
+
+    //Player Animation
+    private Animator playerAnim;
+
 
     private void Awake()
     {
-        //GetComponent() accesses RigidBody2D and stores in rb variable
-        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
 
     private void Update()   //Inputs are recorded every frame
     {
+        //find position of groundCheck object, use players radius(circle around players feet), check for overlap of ground to be true/false
+        surfaced = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         //Moving along x axis
-        float xInp = Input.GetAxisRaw("Horizontal");
+        xInp = Input.GetAxis("Horizontal");
 
-        //Here is where some force is added to the ridged body (rb)
-        rb.velocity = new Vector2(xInp * force, rb.velocity.y);
+        //Here is where some force is added to the ridged body (player)
+        //Moving left or right on x axis
+        if(xInp > 0f){ 
+            player.velocity = new Vector2(xInp * force, player.velocity.y);
+        }
+        else if(xInp < 0f){
+            player.velocity = new Vector2(xInp * force, player.velocity.y);    
+        }
+        else{ //not moving
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
 
-        /*
-        //Make the player turn/flip when moving left and right
-        if (xInp > 0.01f)
-            transform.localScale = Vector3.one;
-        else if (xInp < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
-        */
-
-        //Detect if the space key is pressed down
+        //Detect if the space key is pressed down and player is in contact with ground
+        //If both are true, the player jumps
         if (Input.GetKeyDown(KeyCode.Space) && surfaced)
         {
-            Jump(); //Player/rigidbody/bird jumps                                                
+            player.velocity = new Vector2(player.velocity.x, force); //Player jump mechanic                                               
         }
-    }
 
-    private void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, force); //Player jump mechanic
-        //Player is not touching floor
-        surfaced = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Check for player contact with floor/ground
-        if(collision.gameObject.tag == "Floor")
-        {
-            //Player is in contact with floor
-            surfaced = true;
-        }
+        //update speed operator in animator
+        playerAnim.SetFloat("Force", Mathf.Abs(player.velocity.x));
+        //update boolean for player on ground or not
+        playerAnim.SetBool("OnGround", surfaced);
     }
 }
